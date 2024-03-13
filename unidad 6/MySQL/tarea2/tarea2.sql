9739d197a671567e8b208abc02de5cadbc49b3a4
Chugani05 <chugani205@gmail.com>
2024-03-13 19:07:53 +0000

    arreglando

--- unidad 6/MySQL/tarea2/tarea2.sql
+++ unidad 6/MySQL/tarea2/tarea2.sql
@@ -95,31 +95,30 @@ CREATE DATABASE tarea2
 USE tarea2
 
 
--- Rrealiza las siguientes consultas:
-
+-- Realiza las siguientes consultas:
 -- Consultas simples:
--- Mostrar todos los clientes.
--- Mostrar todos los productos.
--- Mostrar todas las órdenes.
--- Mostrar los primeros 5 clientes ordenados por nombre.
--- Mostrar los productos con un precio mayor a 50.
--- Mostrar todas las órdenes realizadas por el cliente con ID 1.
--- Mostrar los clientes cuyos nombres comienzan con la letra "M".
--- Mostrar las órdenes que contienen más de 2 productos.
--- Mostrar los productos ordenados por precio de forma descendente.
+-- 1. Mostrar todos los clientes.
+-- 2. Mostrar todos los productos.
+-- 3. Mostrar todas las órdenes.
+-- 4. Mostrar los primeros 5 clientes ordenados por nombre.
+-- 5. Mostrar los productos con un precio mayor a 50.
+-- 6. Mostrar todas las órdenes realizadas por el cliente con ID 1.
+-- 7. Mostrar los clientes cuyos nombres comienzan con la letra "M".
+-- 8. Mostrar las órdenes que contienen más de 2 productos.
+-- 9. Mostrar los productos ordenados por precio de forma descendente.
 
 -- Consultas de ejemplo para practicar joins
--- Seleccionar todos los clientes y sus órdenes, incluso si no tienen órdenes
--- Seleccionar todas las órdenes junto con los productos correspondientes
--- Mostrar el nombre de los clientes que han realizado órdenes de productos que cuestan más de 50
--- Obtener el nombre de los productos que no se han ordenado aún
--- Mostrar el nombre del cliente, el producto y la cantidad para todas las órdenes
--- Obtener el nombre de los productos junto con los nombres de los clientes que han realizado órdenes de esos productos
--- Mostrar todas las órdenes con sus clientes y productos, incluso si no hay órdenes
--- Obtener el nombre de los clientes junto con el número total de órdenes que han realizado
--- Mostrar todas las órdenes junto con el nombre del cliente y el nombre del producto
--- Mostrar todas las órdenes con sus productos y clientes, incluso si no hay información de cliente.
--- Obtener el nombre de los productos junto con el nombre de los clientes que han realizado órdenes de esos productos, incluyendo los productos que no han sido ordenados
--- Mostrar todas las órdenes junto con el nombre del cliente y el nombre del producto, incluyendo las órdenes sin productos
--- Obtener el nombre de los clientes junto con el número total de órdenes que han realizado, incluyendo los clientes que no han realizado órdenes.
--- Mostrar todas las órdenes con sus clientes y productos, incluyendo las órdenes y productos que no tienen información.
+-- 1. Seleccionar todos los clientes y sus órdenes, incluso si no tienen órdenes
+-- 2. Seleccionar todas las órdenes junto con los productos correspondientes
+-- 3. Mostrar el nombre de los clientes que han realizado órdenes de productos que cuestan más de 50
+-- 4. Obtener el nombre de los productos que no se han ordenado aún
+-- 5. Mostrar el nombre del cliente, el producto y la cantidad para todas las órdenes
+-- 6. Obtener el nombre de los productos junto con los nombres de los clientes que han realizado órdenes de esos productos
+-- 7. Mostrar todas las órdenes con sus clientes y productos, incluso si no hay órdenes
+-- 8. Obtener el nombre de los clientes junto con el número total de órdenes que han realizado
+-- 9. Mostrar todas las órdenes junto con el nombre del cliente y el nombre del producto
+-- 10. Mostrar todas las órdenes con sus productos y clientes, incluso si no hay información de cliente.
+-- 11. Obtener el nombre de los productos junto con el nombre de los clientes que han realizado órdenes de esos productos, incluyendo los productos que no han sido ordenados
+-- 12. Mostrar todas las órdenes junto con el nombre del cliente y el nombre del producto, incluyendo las órdenes sin productos
+-- 13. Obtener el nombre de los clientes junto con el número total de órdenes que han realizado, incluyendo los clientes que no han realizado órdenes.
+-- 14. Mostrar todas las órdenes con sus clientes y productos, incluyendo las órdenes y productos que no tienen información.
